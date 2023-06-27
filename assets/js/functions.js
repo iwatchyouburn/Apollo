20f73e3548042832a8d55d2b5cd5bc34e8279f2f
Kevin Bouhalassae <bouhalassak@gmail.com>
2023-06-27 12:44:04 -0400

    slider OK

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -13,3 +13,43 @@ function nav(){
 nav();
 
 });
+
+const previousBtn = document.querySelector(".icon-chevron-thin-left.prev");
+const nextBtn = document.querySelector(".icon-chevron-thin-right.next");
+const imageActive = document.querySelectorAll(".banner > li");
+
+// console.log(imageActive)
+
+nextBtn.addEventListener('click', function () {
+	// console.log(nextBtn)
+	let index = -1;
+	imageActive.forEach((btn, indexButton) => {
+	if (btn.classList.contains('active')) {
+		index = indexButton;
+		return 
+	 } 
+	 
+	})
+	imageActive[index].classList.remove('active');
+
+	const nextIndex = (index + 1) % imageActive.length;
+
+    imageActive[nextIndex].classList.add('active');
+})
+
+previousBtn.addEventListener('click', function () {
+	// console.log(previousBtn)
+	let index = -1;
+	imageActive.forEach((btn, indexButton) => {
+	if (btn.classList.contains('active')) {
+		index = indexButton;
+		return 
+	 } 
+	 
+	})
+	imageActive[index].classList.remove('active');
+
+	const prevIndex = (index - 1 + imageActive.length) % imageActive.length;
+
+    imageActive[prevIndex].classList.add('active');
+})

43a953b073308883b0e59f04785057b86cf4bb3c
Kevin Bouhalassae <bouhalassak@gmail.com>
2023-06-28 17:39:25 -0400

    map OK

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -53,3 +53,11 @@ previousBtn.addEventListener('click', function () {
 
     imageActive[prevIndex].classList.add('active');
 })
+
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
